a21df2cf931f78001352a85df0e26fdeb5719229
glenn <glenn.jo@kakaocorp.com>
2020-12-01 21:30:45 +0900

    first commit

---
+++ src/components/Board.vue
@@ -0,0 +1,38 @@
+<template>
+    <table>
+        <tr>
+            <td>이름</td>
+            <td>나이</td>
+            <td>가입일</td>
+        </tr>
+
+        <tr>
+            <td>Glenn</td>
+            <td>20</td>
+            <td>2020.12.01</td>
+        </tr>
+
+        <tr>
+            <td>Glenn2</td>
+            <td>20</td>
+            <td>2020.12.01</td>
+        </tr>
+    </table>
+  
+</template>
+
+<script>
+export default {
+  name: 'Board',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+table {
+  border: 0px;
+}
+</style>
